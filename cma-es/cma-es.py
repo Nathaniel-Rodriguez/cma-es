5f86bc79f789f0974f6c798963b3f8017dfc434d
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2017-02-07 14:23:16 -0500

    added boundary handling functions

--- cma-es/cma-es.py
+++ cma-es/cma-es.py
@@ -9,7 +9,7 @@ class CMAEvolutionaryStrategy:
     Hansen, N. (2016). The CMA evolution strategy: A tutorial. arXiv:1604.00772. https://doi.org/10.1007/11007937_4
     DEAP: https://github.com/DEAP/deap/blob/master/deap/cma.py
 
-    Any functions that are to be optimized MUST be pickleable (serializable).
+    Any functions that are to be optimized MUST be pickleable (serializable). These functions will be minimized.
     """
 
     def __init__(self, x0, sigma0, **kwargs):
@@ -34,7 +34,7 @@ class CMAEvolutionaryStrategy:
         self.population_size = kwargs.get('population_size', 4 + math.floor(3 * math.log(self.num_of_dimensions)))
         self.num_of_parents = kwargs.get('num_of_parents', self.population_size / 2.)
         self.update_count = 0
-        self.compute_parameters(kwargs)
+        self._compute_parameters(kwargs)
 
         # Logging variables
         # Population history is a nested list. The outer list is over generations, the inner list is sorted by
@@ -42,9 +42,12 @@ class CMAEvolutionaryStrategy:
         self.population_history = []
         self.all_time_best = {} # keyed by 'x' and 'fitness'
 
-    def compute_parameters(self, **kwargs):
+    def _compute_parameters(self, **kwargs):
 
-        self.effective_sigma = self.sigma0 * self.scaling_of_variables
+        self.sigma = sigma
+
+        ### MIght need effective_sigma back
+
         self.chiN = math.sqrt(self.dim) * (1 - 1. / (4. * self.dim) + 1. / (21. * self.dim ** 2))
         # Generate initial evolutionary paths
         self.cov_matrix_path = np.zeros(self.num_of_dimensions)
@@ -74,25 +77,25 @@ class CMAEvolutionaryStrategy:
         # Damping term for sigma
         self.sigma_damping = 1. + 2 * max(0, math.sqrt((self.mu_effective - 1) / (self.num_of_dimensions + 1)) - 1) + self.sigma_time_const
 
-    def generate_population(self):
+    def _generate_population(self):
         """
         Samples new members from the distribution using the current centroid and covariance matrix
         """
 
-        return self.centroid + self.effective_sigma * \
+        return self.centroid + self.sigma * self.scaling_of_variables * \
             np.dot(np.random.standard_normal((self.population_size, self.num_of_dimensions)), self.BD.T)
 
     def _updated_sigma_path(self, c_diff):
 
         self.sigma_path = (1 - self.sigma_time_const) * self.sigma_path \
-            + math.sqrt(self.sigma_time_const * (2 - self.sigma_time_const) * self.mu_effective) / self.effective_sigma \
+            + math.sqrt(self.sigma_time_const * (2 - self.sigma_time_const) * self.mu_effective) / self.sigma \
             * np.dot(self.B, (1. / self.diagD)
                         * np.dot(self.B.T, c_diff))
 
     def _update_cov_path(self, c_diff, hsig):
 
         self.cov_matrix_path = (1 - self.cov_time_const) * self.cov_matrix_path + hsig \
-            * sqrt(self.cov_time_const * (2 - self.cov_time_const) * self.mu_effective) / self.effective_sigma \
+            * sqrt(self.cov_time_const * (2 - self.cov_time_const) * self.mu_effective) / self.sigma \
             * c_diff
 
     def _update_cov_matrix(self, parent_dist_from_centroid, hsig):
@@ -101,11 +104,11 @@ class CMAEvolutionaryStrategy:
                   * self.cov_1 * self.cov_time_const * (2 - self.cov_time_const)) * self.covariance_matrix \
             + self.cov_1 * numpy.outer(self.cov_matrix_path, self.cov_matrix_path) \
             + self.cov_mu * numpy.dot((self.weights * parent_dist_from_centroid.T), parent_dist_from_centroid) \
-            / self.effective_sigma ** 2
+            / self.sigma ** 2
 
     def _update_sigma(self):
 
-        self.effective_sigma *= np.exp((np.linalg.norm(self.sigma_path) / self.chiN - 1.)
+        self.sigma *= np.exp((np.linalg.norm(self.sigma_path) / self.chiN - 1.)
                                 * self.sigma_time_const / self.sigma_damping)        
 
     def _update_eigen_decomposition(self):
@@ -116,23 +119,40 @@ class CMAEvolutionaryStrategy:
         self.B = self.B[:, indx]
         self.BD = self.B * self.diagD      
 
-    def update(self):
+    def _update_log(self, population, fitness_values):
         """
+        Adds the new population and fitness values to the population_history
+        """
+
+        self.population_history.append({ 'x' : population[i], 'fitness': fitness_values[i]} for i in range(self.population_size) )
+
+        if len(self.all_time_best) == 0:
+            self.all_time_best['x'] = self.population_history[-1][0]['x']
+            self.all_time_best['fitness'] = self.population_history[-1][0]['fitness']
+        elif self.all_time_best['fitness'] < self.population_history[-1][0]['fitness']:
+            self.all_time_best['x'] = self.population_history[-1][0]['x']
+            self.all_time_best['fitness'] = self.population_history[-1][0]['fitness']
+
+    def _update(self, objective_funct, args):
+        """
+        updates the evolutionary paths, sigma, and the covariance matrix.
         """
 
         # Sample from distribution to create new offspring and Evaluate fitness values
-        population = generate_population()
-        fitness_values = Parallel(n_jobs=num_of_jobs)(delayed(objective_funct)(pop, *args) for pop in population )
+        population = self._generate_population()
+        valid_population, violations = self._boundary_handling()
+        fitness_values = Parallel(n_jobs=num_of_jobs)(delayed(objective_funct)(pop, *args) for pop in valid_population )
+        corrected_fitness = self._boundary_correction(fitness_values, violations)
 
         # Sort the results (both values and population)
-        fitness_sorted_g2l, population_sorted_by_fitness = zip(*sorted(zip(fitness_values, population), reversed=True))
-        self.update_log(population_sorted_by_fitness, fitness_sorted_g2l)
+        fitness_sorted_g2l, population_sorted_by_fitness = zip(*sorted(zip(corrected_fitness, population), reversed=True))
+        self._update_log(population_sorted_by_fitness, fitness_sorted_g2l)
 
         # Calculate the weighted means of the ranked, centroid difference, and h_sig
         old_centroid = self.centroid.copy()
         self.centroid = np.dot(self.weights, population[0:self.num_of_parents])
         c_diff = self.centroid - old_centroid
-        hsig = float((np.linalg.norm(self.sigma_path) /# might get broken if sigma path is a vector
+        hsig = float((np.linalg.norm(self.sigma_path) /
                       math.sqrt(1. - (1. - self.sigma_time_const) ** (2. * (self.update_count + 1.))) / self.chiN
                       < (1.4 + 2. / (self.dim + 1.))))
         self.update_count += 1
@@ -149,48 +169,100 @@ class CMAEvolutionaryStrategy:
         # Update eigen decomposition
         self._update_eigen_decomposition()
 
-    def boundary_handling():
+    def _nearest_valid_parameters(self, invalid_parameters, bounds):
         """
+        Calculates the set of nearest valid parameters given some
+        invalid set and the bounds. Returns a valid parameter set
+        and the distance between the new and old set, and a flag
+        that states whether any invalid parameters were found
+
+        The flag is to avoid float point equivalence comparisons
         """
 
-        pass
+        valid_parameters = invalid_parameters.copy()
+        is_valid = True
+        for i in range(self.num_of_dimensions):
+            if invalid_parameters[i] < bounds[i][0]:
+                valid_parameters[i] = bounds[i][0]
+                is_valid = False
+            elif invalid_parameters[i] > bounds[i][1]:
+                valid_parameters[i] = bounds[i][1]
+                is_valid = False
 
-    def eval():
+        return valid_parameters, np.linalg.norm(valid_parameters - invalid_parameters), is_valid
 
-        pass
+    def _boundary_handling(self, population):
+        """
+        Check population for invalide members.
+        Revise member by placing in valid region.
+        Return a list of revised population, a list of tuples with indexes of violated members
+        and the distances between the invalid and validified members
+        """
 
-    def engage(self, objective_funct, args, iterations = 100, parallel=True, num_of_jobs=-2):
+        if self.bounds == None:
+            return population, [ (False,0) for i in range(self.population_size) ]
+
+        violations = []
+        valid_population = population.copy()
+        for i, member in enumerate(population):
+            valid_member, distance, validity = self._nearest_valid_parameters(member, self.bounds)
+            valid_population[i] = valid_member
+            violations.append((validity, distance))
+
+        return valid_population, violations
+
+    def _boundary_correction(self, fitness, violations):
+        """
+        Apply corrections to the fitness of members that violated conditions
+
+        This is just an ad hoc adjustment, will implement the following in the 
+        future: Errata/Addenda for A Method for Handling
+        Uncertainty in Evolutionary Optimization With an
+        Application to Feedback Control of Combustion, Nikolaus Hansen, 2011
+        """
+
+        corrected_fitness = []
+        for i in range(self.population_size):
+            if violations[i][0]:
+                corrected_fitness.append(fitness[i] + 
+                    violations[i][1] * np.median(fitness) / (self.sigma * self.sigma * np.mean(covariance_matrix)))
+            else:
+                corrected_fitness.append(fitness[i])
+
+        return corrected_fitness
+
+    def engage(self, objective_funct, args, iterations = 100, parallel=True, 
+        num_of_jobs=-2, bounds=None, verbose=False):
         """
         Run the update process on the objective function for designated number of iterations.
         If num_of_jobs = -2, one less than max number of processes are run
         If num_of_jobs = -1, max number of processes are run
         If num_of_jobs = 1, it will be run in serial.
+
+        bounds: list of tuples of (min,max) values for each parameter
         """
 
-        pass
+        self.bounds = bounds
+        if parallel:
+            self.num_of_jobs = num_of_jobs
+        else:
+            self.num_of_jobs = 1
 
-    def update_log(self, population, fitness_values):
-        """
-        Adds the new population and fitness values to the population_history
-        """
+        for i in range(iterations):
+            if verbose:
+                print("Generation: ", i)
+            self._update(objective_funct, args)
 
-        self.population_history.append({ 'x' : population[i], 'fitness': fitness_values[i]} for i in range(self.population_size) )
-
-        if len(self.all_time_best) == 0:
-            self.all_time_best['x'] = self.population_history[-1][0]['x']
-            self.all_time_best['fitness'] = self.population_history[-1][0]['fitness']
-        elif self.all_time_best['fitness'] < self.population_history[-1][0]['fitness']:
-            self.all_time_best['x'] = self.population_history[-1][0]['x']
-            self.all_time_best['fitness'] = self.population_history[-1][0]['fitness']
-
-def fmin(objective_funct, x0, sigma0, args=(), iterations=1000, parallel=True, num_of_jobs=-2, cma_params={}, bounds=[]):
+def fmin(objective_funct, x0, sigma0, args=(), iterations=1000, parallel=True, num_of_jobs=-2, 
+    cma_params={}, bounds=[], verbose=False):
     """
     A functional version of the CMA evolutionary strategy
 
     bounds: a list of tuples in the order of x0. Tuple are the ranges of the parameters accepted.
     """
 
-    pass
+    cma_object = CMAEvolutionaryStrategy(x0, sigma0, **cma_params)
+    cma.engage(objective_funct, args, iterations, parallel, num_of_jobs, bounds, verbose)
 
 if __name__ == '__main__':
     """
